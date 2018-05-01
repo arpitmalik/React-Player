670226682519f2ef508373a412a7737e9d145050
Arpit Malik <arpitmalik04@gmail.com>
2018-05-01 22:37:56 +0530

    React app

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import _ from 'lodash';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list'
+import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyBC7OlOPIBID0Z65nj3enTOALtjVst_9tA'; // YouTube API key
+
+// Parent Component
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			videos: [] ,
+			selectedVideo: null
+		};
+		this.videoSearch('comedy standups')
+	} 
+
+	videoSearch(term){
+		YTSearch({key: API_KEY, term:term},(videos) => {
+			this.setState({
+				videos:videos,
+				selectedVideo: videos[0]
+			});
+		});
+
+	}
+	render(){
+		const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
+		return ( 
+			<div>
+				<h1 className="heading">Y*utube</h1>
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList 
+				onVideoSelect = {selectedVideo => this.setState({selectedVideo})}				
+				videos={this.state.videos} />
+			</div >
+		);
+	}
+}
+
+// Rendering
+ReactDOM.render(<App />, document.querySelector('.container'));
